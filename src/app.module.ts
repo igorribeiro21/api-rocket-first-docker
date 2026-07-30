60df694681ff760220ced233bcee5e3efbd71b11
igor.ribeiro <igorapalvesribeiro@gmail.com>
2026-07-29 21:54:45 -0300

    feat: added configuration database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: 'root',
+      database: 'rocketseat-db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
